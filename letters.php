d2b069f6e384ff98f803ee4c297b6c76d38f206d
golinskis <simon.golinski@gmail.com>
2017-12-14 21:58:39 +0100

    Funkcja dodajaca ciag zankow na poczatku stringu

---
+++ letters.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Skrypt zawiera kilka ćwiczeniowych funkcji operujących na stringach.
+ *
+ * Przykład użycia:
+ * 1. Zdefiniuj zmienną $stringToTest na ciąg znaków, który chcesz przetestować.
+ * 2. Wywołuj kolejne funkcje zgodnie z ich dokumentacją.
+ * 3. Uruchom skrypt w konsoli: php -f letters.php
+ */
+
+declare(strict_types=1);
+
+$stringToTest = 'rabbit';
+
+removeLastLetter($stringToTest);
+appendLetters($stringToTest, 's');
+insertLetters($stringToTest,'my');
+/**
+ * Dodanie do tekstu na końcu ciągu znaków.
+ *
+ * @param string $text Tekst, do którego dodajemy ciąg znaków.
+ * @param string $letters Ciąg znaków do dodania.
+ *
+ * @return string Tekst z dodanymi literami.
+ */
+function appendLetters(string $text, string $letters): string
+{
+    echo "Before ", __FUNCTION__, ": ", $text, PHP_EOL;
+    $text .= $letters;
+    echo "After ", __FUNCTION__, ": ", $text, PHP_EOL;
+
+    return $text;
+}
+echo "<br>";
+/**
+ * Usunięcie ostatniej litery z podanego ciągu znaków.
+ *
+ * @param string $text Tekst, z którego usuniemy ostatnią literę.
+ *
+ * @return string Tekst bez ostatniej litery.
+function insertLetters (string $text,string $letters)
+{
+echo  "Before", __FUNCTION__,": ",$string,PHP_EOL;
+$letters.=$text;
+$text=$letters;
+echo "After",__FUNCTION__,": ",$text,PHP_EOL;
+
+return $text;
+ */
+function removeLastLetter(string $text): string
+{
+    echo "Before ", __FUNCTION__, ": ", $text, PHP_EOL;
+    $text = substr($text, 0, strlen($text) - 1);
+    echo "After ", __FUNCTION__, ": ", $text, PHP_EOL;
+
+    return $text;
+}
+
+/**
+ * Dodanie do tekstu  przed ciągiem znaków.
+ *
+ *@param string $text  tekst do którego doajemy ciąg znaków.
+ *$param string $letters ciąg znaków do dodania.
+ *
+ *$return string Tekst z dodanymi literami.
+*/
+function insertLetters (string $text,string $letters)
+{
+echo  "Before", __FUNCTION__,": ",$text,PHP_EOL;
+$letters.=$text;
+$text=$letters;
+echo "After",__FUNCTION__,": ",$text,PHP_EOL;
+
+return $text;
+
+}
+
+
