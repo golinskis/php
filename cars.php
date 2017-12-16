45bd96b8248918d8e88e43be4117a030b06779c0
golinskis <simon.golinski@gmail.com>
2017-12-16 11:21:40 +0100

    Szukanie indexu

---
+++ cars.php
@@ -0,0 +1,14 @@
+<?php
+
+$cars = [
+	'volvo' => [
+		'xc90','xc60','v60',
+	],
+	'skoda' => [
+		'felicia' =>[
+			'hatchback','kombi','pickup',
+			],
+		],
+	];	
+
+var_export(isset($cars['mercedes']));
