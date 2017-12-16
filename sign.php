60742d575624753337d4e9db7aa25bfee214c7dc
golinskis <simon.golinski@gmail.com>
2017-12-16 09:23:47 +0100

    rozwiazanie zadanie pierwszego

---
+++ sign.php
@@ -0,0 +1,7 @@
+<?php
+
+$number=2;
+
+$number>0 ? echo   "Liczba jest dodatnia" : echo "Liczba -3 jest ujemna";
+$number=-3;
+$number==-3 ? echo  "Liczba -3 jest ujemna" : echo "Liczba 2 jest dodatnia";
