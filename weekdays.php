6f4bf905bfa873fd3c040dd2e667fb31f05889da
golinskis <simon.golinski@gmail.com>
2017-12-16 11:31:28 +0100

    Wprowadzenie drugiego wymiaru

---
+++ weekdays.php
@@ -0,0 +1,26 @@
+
+<?php
+
+$weekdays=[
+ 	pl =>[ 
+    		'monday'=>'poniedziałek',
+    		'thuesday'=>'wtorek',
+   		'wednesday'=>'sroda',
+    		'thursday'=>'czwartek',
+    		'friday'=>'piatek',
+    		'saturday'=>'sobota',
+    		'sunday'=>'niedziela',
+	],
+	de => [
+    		'monday'=>'montag',
+    		'thuesday'=>'dienstag',
+    		'wednesday'=>'mittwoch',
+    		'thursday'=>'donnerstag',
+    		'friday'=>'freitag',
+    		'saturday'=>'samstag',
+    		'sunday'=>'sonntag',
+	],
+ ];
+
+var_export($weekdays);
+~
