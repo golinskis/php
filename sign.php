f8e71f14d2ed86edd8bf152a9fde76c4eab19d2e
golinskis <simon.golinski@gmail.com>
2017-12-16 09:38:12 +0100

    Poprawne rozwiazanie zadania

--- sign.php
+++ sign.php
@@ -2,6 +2,5 @@
 
 $number=2;
 
-$number>0 ? echo   "Liczba jest dodatnia" : echo "Liczba -3 jest ujemna";
-$number=-3;
-$number==-3 ? echo  "Liczba -3 jest ujemna" : echo "Liczba 2 jest dodatnia";
+$ternarius=$number>0 ? "Dodatnia" : " Ujemna";
+echo $number." jest ".$ternarius; 
