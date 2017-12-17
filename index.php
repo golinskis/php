aff70479d9ba84628e238cdee17dfd2e07da05b5
golinskis <simon.golinski@gmail.com>
2017-12-17 15:44:38 +0100

    Setery i getery

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require_once 'Customer.php';
 $customer = new Customer();
-$customer->name = 'Marian';
+$customer->setName('Marian');
 echo $customer->getDiscount();
+echo $customer->name;
 var_export($customer);
